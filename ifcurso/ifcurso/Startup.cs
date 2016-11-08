7f52c4dfcacb3ea90896b825f1aa11ddc14b3042
edu-ricardo <edu_ricardo@live.com>
2016-11-08 20:12:00 -0200

    Initial Commit

---
+++ ifcurso/ifcurso/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ifcurso.Startup))]
+namespace ifcurso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
